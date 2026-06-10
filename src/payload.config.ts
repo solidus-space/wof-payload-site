dc0c5fd96095199b7301c1f46a7e39c99e903668
Marcus Hirst <hello@marcushirst.com>
2026-06-10 22:20:52 +0100

    Declare CSS module

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -36,7 +36,7 @@ const cloudflareLogger = {
   error: createLog('error', console.error),
   fatal: createLog('fatal', console.error),
   silent: () => {},
-} as any // Use PayloadLogger type when it's exported
+} // eslint-disable-next-line @typescript-eslint/no-explicit-any
 
 const cloudflare =
   isCLI || !isProduction
